e9a5ba6a8b127c648793fcd168595e8a035d0a1c
Lydia-coder <lydiatjuh@gmail.com>
2020-07-13 16:01:18 +0200

    changed styling in details

--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -1,70 +1,75 @@
 <template>
-  <div class="outline">
-    <b-carousel
-      id="carousel-1"
-      :interval="8000"
-      controls
-      indicators
-      background="rgb(232, 158, 12)"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-    >
-      <!-- @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"-->
+  <div>
+    <Header />
+    <div class="outline">
+      <b-carousel
+        id="carousel-1"
+        :interval="8000"
+        controls
+        indicators
+        background="rgb(232, 158, 12)"
+        img-width="1024"
+        img-height="480"
+        style="text-shadow: 1px 1px 2px #333;"
+      >
+        <!-- @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"-->
 
-      <b-carousel-slide>
-        <template v-slot:img>
+        <b-carousel-slide>
+          <template v-slot:img>
+            <img
+              class="d-block img-fluid w-100"
+              width="1024"
+              height="480"
+              :src="currentBeer.image_url"
+              :alt="currentBeer.name"
+            />
+          </template>
+        </b-carousel-slide>
+
+        <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+        <b-carousel-slide img-blank :img-alt="currentBeer.name">
+          <h3>{{currentBeer.name}}</h3>
           <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            :src="currentBeer.image_url"
-            :alt="currentBeer.name"
+            src="https://blog.joypixels.com/content/images/2019/12/clinking_beer_mugs-1.gif"
+            alt="beer"
           />
-        </template>
-      </b-carousel-slide>
+          <div>
+            <h4>ABV:</h4>
+            <li>{{currentBeer.abv}}%</li>
+          </div>
 
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide img-blank :img-alt="currentBeer.name">
-        <h3>{{currentBeer.name}}</h3>
-        <img
-          src="https://blog.joypixels.com/content/images/2019/12/clinking_beer_mugs-1.gif"
-          alt="beer"
-        />
-        <div>
-          <h4>ABV:</h4>
-          <li>{{currentBeer.abv}}%</li>
-        </div>
+          <h4>First Brewed:</h4>
+          <p>{{currentBeer.first_brewed}}</p>
+        </b-carousel-slide>
 
-        <h4>First Brewed:</h4>
-        <p>{{currentBeer.first_brewed}}</p>
-      </b-carousel-slide>
+        <b-carousel-slide img-blank :img-alt="currentBeer.name">
+          <div>
+            <h4>Description:</h4>
+            <p>{{currentBeer.description}}</p>
+          </div>
 
-      <b-carousel-slide img-blank :img-alt="currentBeer.name">
-        <div>
-          <h4>Description:</h4>
-          <p>{{currentBeer.description}}</p>
-        </div>
-
-        <div>
-          <h4>Brewers Tips:</h4>
-          <p>{{currentBeer. brewers_tips}}</p>
-        </div>
-        <div>
-          <h4>Food Pairing :</h4>
-          <ul v-for="food in currentBeer.food_pairing" :key="food">
-            <li>{{food}}</li>
-          </ul>
-        </div>
-      </b-carousel-slide>
-    </b-carousel>
+          <div>
+            <h4>Brewers Tips:</h4>
+            <p>{{currentBeer. brewers_tips}}</p>
+          </div>
+          <div>
+            <h4>Food Pairing :</h4>
+            <ul v-for="food in currentBeer.food_pairing" :key="food">
+              <li>{{food}}</li>
+            </ul>
+          </div>
+        </b-carousel-slide>
+      </b-carousel>
+    </div>
   </div>
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
+import Header from "./Header";
 export default {
   name: "BeerDetail",
+  components: { Header },
   computed: mapGetters(["currentBeer", "allBeers"]),
 
   methods: {
@@ -113,15 +118,13 @@ export default {
 #carousel-1 {
   margin: 70px;
   outline: 2px solid orange;
-  outline-offset: 20px;
+  outline-offset: 50px;
 }
 .d-block {
   margin-top: 10px;
   margin-bottom: 6px;
 }
 .outline {
-  border: 2px solid orange;
-  margin: 20px;
 }
 
 h3 {
@@ -144,9 +147,11 @@ p ::before {
 
 p {
   font-family: "Indie Flower", cursive;
+  font-size: 20px;
 }
 ul {
   font-family: "Indie Flower", cursive;
+  font-size: 20px;
 }
 </style>
 
